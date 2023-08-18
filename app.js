4e107bf58b571655cf59075aa2cc0f4296f2b846
GuHyunWoo <rkwhr99@gmail.com>
2023-08-18 10:38:21 +0900

    ws 추가

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//인터넷 페이지
+
 const express = require('express');
 const usersRouter = require('./usersRouter');
 
@@ -17,3 +19,27 @@ app.listen(port, ()=>
 {
    console.log(`SERVER 실행됨 ${port}`); 
 });
+
+//웹소켓
+const wsServer = require('ws').Server;
+var wss = new wsServer({ port: 4000});
+
+//https://jdh5202.tistory.com/886 참고사이트
+wss.on('connection', function connection(client)
+{
+    console.log('Client connected');
+
+    client.send("ServerConnection");
+});
+
+//클라이언트로부터의 메세지 처리
+wss.on('message',(msg) => 
+{
+
+})
+
+// 연결 종료 처리
+wss.on('close',() => 
+{
+
+})
