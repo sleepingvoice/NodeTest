35ac7e7b299c88fb02d5cad4dad589227a55ab45
GuHyunWoo <rkwhr99@gmail.com>
2023-10-04 10:00:30 +0900

    이미지 부분 삭제

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const divide = require('./messageDivide');
 
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' })
-const sqlConnect = require('./sqlConnect');
-
 const app = express();
 const port = 5000;
 
@@ -18,47 +14,6 @@ app.get('/', (req, res)=>
 app.listen(port, ()=>
 {
    console.log(`SERVER 실행됨 ${port}`);
-});
-
-app.post('/imgfile',upload.single('image') ,(req,res) =>
-{
-    if (!req.file) {
-        console.log("Image File Not Found");
-        return;
-    }
-
-    const image = req.file;
-    const imagePath = image.path;
-
-    const insertQuery = 'INSERT INTO imgtable (img) VALUES' + '(\'' + imagePath + '\')';
-
-    sqlConnect.MessageQuery(insertQuery,(result) =>
-    {
-        console.log('Add ImageFile');
-        res.send(result.insertId.toString());
-    })
-})
-
-app.get('/getImage/:imageId', (req, res) => {
-    const imageId = req.params.imageId;
-    const insertQuery = `SELECT * FROM imgtable WHERE imgId = ${imageId}`;
-    
-    sqlConnect.MessageQuery(insertQuery,(result) =>
-    {
-        if (result.length === 0) {
-            console.log(insertQuery);
-            console.log("Image File Not Found");
-            return;
-        }
-
-        var path = result[0].img;
-        path = path.replace('uploads','uploads/');
-        
-        console.log("Image File Send");
-        
-        res.sendFile(path, { root: __dirname });
-    })
-
 });
 
 //웹소켓
